289e45d6a14d9cd1b56d5915584cdebae4ea9588
LukaZimer <lukazimer16@gmail.com>
2023-01-25 21:03:58 +0100

    commit 1

---
+++ script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "New Heading";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+ 
+
+
+
+
+
+function mojaFunkcja(){
+
+    // cialo funkcji
+    
+}
